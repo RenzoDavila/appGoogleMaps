b27a96dcb2cafb777979dc83abd92defb6fcd89a
USERSAD\rdavilac <rdavilac@everis.com>
2022-01-11 19:02:08 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { MapInfoWindow, MapMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'appGoogleMaps';
+  title = 'Como usar el Componente Google Maps de Angular';
+
+  // Configuración de Google Maps
+  center = { lat: -11.9865699, lng: -77.0679477 };
+  zoom = 15;
+  display?: google.maps.LatLngLiteral;
 }
